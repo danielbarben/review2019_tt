e8f177acaa043ed95f0438995c39bbf7e187f3a9
Daniel Barben <daniel.barben@tamedia.ch>
2019-12-26 09:24:49 +0100

    new text and image

--- src/Components/Event/index.js
+++ src/Components/Event/index.js
@@ -20,7 +20,7 @@ open = () => {
     return (
     <li>
       <div className = "date" onClick = {() => this.open()}>{this.props.date} {this.state.visibility ?  <FaCaretUp /> : <FaCaretDown />}<h3 className = "headline">{this.props.headline}</h3></div>
-      <p className = {this.state.visibility ? "text visible" : "text hidden"}>{this.props.image !== "" ? <img src={gallery[this.props.image]} alt={this.props.headline} className="image"/> : ""}<br/>{this.props.text}
+      <p className = {this.state.visibility ? "text visible" : "text hidden"}>{this.props.image !== "" ? <img src={gallery[this.props.image]} alt={this.props.headline} className="image"/> : ""}{this.props.text}
       </p>
     </li>
   )}
